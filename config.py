f10e9d4f7058ad024237a5a0728a35360cb6d4c3
ravraham <ravraham@vmware.com>
2019-08-12 20:11:04 +0300

    add support for postgres db

---
+++ config.py
@@ -0,0 +1,27 @@
+# !/usr/bin/python
+from configparser import ConfigParser
+import os
+
+
+def config(filename='database.ini', section='postgresql-local'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_ECHO = False
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/postgres"
--- github_repos.py
+++ github_repos.py
@@ -1,7 +1,45 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from config import config
 import logging
 import GitHub
 
 repos_dict = {}
+
+
+def get_query(query):
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        cur.execute(query)
+        print("Count of records: ", cur.rowcount)
+        return cur.fetchall()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def put_query(query):
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        cur.execute(query)
+        conn.commit()
+        return 'success'
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return error
+    finally:
+        if conn is not None:
+            conn.close()
 
 
 def query_github(key):
@@ -11,6 +49,12 @@ def query_github(key):
         repos_dict[user['id']] = {'name': user['name'],
                                   'stars_count': user['stargazers_count'],
                                   'language': user['language']}
+        query = "INSERT INTO github (name, stars, language) " \
+                "VALUES('{name}', '{stars}', '{language}');". \
+            format(name=user['name'],
+                   stars=user['stargazers_count'],
+                   language=user['language'])
+        put_query(query)
 
 
 def main():
@@ -20,6 +64,8 @@ def main():
         sorted_keys = sorted(repos_dict, key=lambda x: (repos_dict[x]['stars_count']))
         for i in sorted_keys:
             print (repos_dict[i])
+        query = "select * from test;"
+        print (get_query(query))
     except Exception as e:
         logging.error("{}".format(e))
         exit(1)
